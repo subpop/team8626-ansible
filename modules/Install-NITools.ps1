eeab7bab8ecc1d0effb711c577e261b3e67ac21a
Link Dupont <link@sub-pop.net>
2025-12-31 20:46:12 -0500

    Update list of NI packages

--- modules/Install-NITools.ps1
+++ modules/Install-NITools.ps1
@@ -57,7 +57,7 @@ function Install-NIGameTools {
 
     # Run installer
     Write-Info "Installing NI FRC Game Tools (this takes 15-30 minutes, requires internet)..."
-    Start-Process -FilePath $installerPath -ArgumentList "--accept-eulas --passive install ni-frc-2025-game-tools" -Wait -NoNewWindow
+    Start-Process -FilePath $installerPath -ArgumentList "--accept-eulas --passive install ni-frc-2025-game-tools ni-labview-2023-runtime-engine-x86 ni-certificates ni-vision-common-resources" -Wait -NoNewWindow
     Write-Success "NI FRC Game Tools installed"
 
     # Cleanup
